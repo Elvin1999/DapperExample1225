c56d1c6c8c8edb2086abda8b4e407233fe2d433d
camalzadeelvin <camalzadeelvin@STHQ0118-01>
2023-06-16 11:12:25 +0400

    Add project files.

---
+++ DapperExample1225/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using DapperExample1225.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DapperExample1225
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //GetAllCaller();
+
+
+            var player = GetById(1);
+            myDataGrid.ItemsSource = new List<Player> { player };
+        }
+
+        public async void GetAllCaller()
+        {
+           var players = await GetAllAsync();
+           myDataGrid.ItemsSource = players;
+
+
+        }
+
+        public async Task<List<Player>> GetAllAsync()
+        {
+            List<Player> players = new List<Player>();
+            var conn = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+
+            using (var connection=new SqlConnection(conn))
+            {
+                var data = await connection.QueryAsync<Player>("SELECT Id,Name,Score,IsStar FROM Players");
+                players = data.ToList();
+            }
+            return players;
+        }
+
+        public Player GetById(int id)
+        {
+            var conn = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+            using (var connection=new SqlConnection(conn))
+            {
+                var player = connection
+                    .QueryFirstOrDefault<Player>("SELECT * FROM Players WHERE Id=@PId",new {PId=id });
+                return player;
+            }
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DapperExample1225/SQLQuery1.sql
@@ -0,0 +1,27 @@
+﻿CREATE DATABASE GameDb
+GO
+USE GameDb
+GO
+CREATE TABLE Players(
+Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+Name NVARCHAR(30) NOT NULL,
+Score FLOAT NOT NULL,
+IsStar BIT NOT NULL DEFAULT(0)
+)
+
+GO
+
+INSERT INTO Players([Name],[Score],[IsStar])
+VALUES('Lebron James',99,1),
+('Stephan Curry',95,1),
+('Maykl Jordan',94,0)
+
+GO
+
+SELECT * FROM Players
+
+CREATE PROCEDURE ShowGreaterThan
+(@pScore AS FLOAT)
+AS
+SELECT * FROM Players
+WHERE Score>@pScore
